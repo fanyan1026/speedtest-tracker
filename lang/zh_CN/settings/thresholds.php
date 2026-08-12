d083aacc30675cd199a6dcdd80e4bbe985ef77dc
fanyan1026 <106582609+fanyan1026@users.noreply.github.com>
2026-08-12 22:07:12 +0800

    New translations thresholds.php (Chinese Simplified)
    [ci skip]

--- lang/zh_CN/settings/thresholds.php
+++ lang/zh_CN/settings/thresholds.php
@@ -2,21 +2,21 @@
 
 return [
     'title' => '测速阈值',
-    'label' => 'Thresholds',
+    'label' => '阈值设置',
 
     // Absolute thresholds
-    'absolute' => 'Absolute',
-    'absolute_description' => 'Absolute thresholds do not take into account previous history and could be triggered on each test.',
-    'absolute_enabled' => 'Enable absolute thresholds',
+    'absolute' => '固定阈值',
+    'absolute_description' => '固定阈值不会参考历史测速数据，每次测速只要不达标就会触发告警。',
+    'absolute_enabled' => '启用固定阈值',
 
     // Metrics section
-    'metrics' => 'Metrics',
-    'metrics_helper_text' => 'Set to zero to disable this metric.',
+    'metrics' => '测速指标',
+    'metrics_helper_text' => '填写零代表关闭该指标阈值检测。',
 
     // General threshold labels
-    'thresholds' => 'Thresholds',
-    'threshold_enabled' => 'Threshold enabled',
-    'threshold_download' => 'Threshold download',
-    'threshold_upload' => 'Threshold upload',
+    'thresholds' => '阈值参数',
+    'threshold_enabled' => '启用该阈值',
+    'threshold_download' => '下载速度阈值',
+    'threshold_upload' => '上传速度阈值',
     'threshold_ping' => '延迟阈值',
 ];
